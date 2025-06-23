7322d210a5f6e6406601d2992a8294ba2dfedf2c
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2025-06-24 00:44:40 +0530

    Integration of all the widgets.

---
+++ Golden_ePub_ENHANCED_v2024.e2/OEBPS/widgets/Hotspot/script/hotspot_data.js
@@ -0,0 +1,36 @@
+const main_hotspot_positions = [
+    {
+        "top": "1%",
+        "left": "6%",
+        "width": "11%",
+        "height": "98%"
+    },
+    {
+        "top": "0%",
+        "right": "9.5%",
+        "width": "13%",
+        "height": "107%"
+    },
+    
+]
+
+const tree_hotspot_positions = [
+    {
+        "left": "50%",
+        "top": "2%",
+        "width": "30%",
+        "height": "30%"
+    },
+    {
+        "left": "1%",
+        "bottom": "10%",
+        "width": "48%",
+        "height": "47%"
+    },
+    {
+        "right": "1%",
+        "bottom": "10%",
+        "width": "48%",
+        "height": "47%"
+    }
+]
---
+++ Golden_ePub_ENHANCED_v2024.e2/OEBPS/widgets/inline_questions/data/accordian_audio_data.js
@@ -0,0 +1,70 @@
+const questions_data = {
+    sharedProperties: {
+      maxWidth: "960px", //"100%", "90%", "760px", "1024px"
+      alignment: "center", //"center", "left"
+      pageStateIdentifier: "X1750692051506", //statedata identifier to store data in localstorage.
+      saveStateData: "yes", //Flag to allow save state data or not, values are "yes", "no"
+    },
+    question_groups: {
+      group4: {
+        id: "group4",
+        type: "accordian",
+        placeholder_id: "group4_placeholder",
+        items: [
+          {
+            title: `adenitis`,
+            audio: `../widgets/inline_questions/data/audios/adenitis.mp3`,
+            explanation: `ah-deh-NI-tis`,
+          },
+          {
+            title: `adenoma`,
+            audio: `../widgets/inline_questions/data/audios/adenoma.mp3`,
+            explanation: `ah-deh-NO-mah`,
+          },
+          {
+            title: `adenopathy`,
+            audio: `../widgets//inline_questions/data/audios/adenopathy.mp3`,
+            explanation: `ah-deh-NOP-ah-the`,
+          },
+          {
+            title: `anemia`,
+            audio: `../widgets//inline_questions/data/audios/anemia.mp3`,
+            explanation: `ah-NE-me-ah`,
+          },
+          {
+            title: `arthralgia`,
+            audio: `../widgets//inline_questions/data/audios/arthralgia.mp3`,
+            explanation: `ar-THRAL-jah`,
+          },
+          {
+            title: `arthritis`,
+            audio: `../widgets//inline_questions/data/audios/arthritis.mp3`,
+            explanation: `ar-THRI-tis`,
+          },
+          {
+            title: `autopsy`,
+            audio: `../widgets//inline_questions/data/audios/autopsy.mp3`,
+            explanation: `AW-top-se`,
+          },
+          {
+            title: `biology`,
+            audio: `../widgets//inline_questions/data/audios/biology.mp3`,
+            explanation: `bi-OL-o-je`,
+          },
+          {
+            title: `biopsy`,
+            audio: `../widgets//inline_questions/data/audios/biopsy.mp3`,
+            explanation: `BI-op-se`,
+          },
+          {
+            title: `carcinogenic`,
+            audio: `../widgets//inline_questions/data/audios/carcinogenic.mp3`,
+            explanation: `kar-sih-no-JEN-ik`,
+          }
+        ],
+        titleElement: "h4", //"h1", "h2", "h3", "h4", "h5", "h6" ,""
+        itemSeperator: "false",
+      }
+    },
+  };
+  
