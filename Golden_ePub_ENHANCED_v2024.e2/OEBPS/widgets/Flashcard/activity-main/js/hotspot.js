538cd327672f532891970aac45e4d73640f01d70
motpuse-naveen <89586488+motpuse-naveen@users.noreply.github.com>
2025-12-19 18:15:10 +0530

    Flashcard Changes

--- Golden_ePub_ENHANCED_v2024.e2/OEBPS/widgets/Flashcard/activity-main/js/hotspot.js
+++ Golden_ePub_ENHANCED_v2024.e2/OEBPS/widgets/Flashcard/activity-main/js/hotspot.js
@@ -1,47 +1,48 @@
 var scale = 1
-var hotspot = function(data){
+var hotspot = function (data) {
 
-	var txtdata = jqnc('<div>', {'class':'hotspot-txt'});
-    var imagePreCount = 0;
+	var txtdata = jqnc('<div>', {
+		'class': 'hotspot-txt'
+	});
+	var imagePreCount = 0;
 	var imgPreloadArray = new Array();
 	var hotspotSpaceLeft = 40;
 	var hotspotWidth = 150;
 	var lastFocus = null;
 	var myScroll;
 	var loadingDiv;
-    function preloadImage(){
-    	jqnc(data).css('visibility','hidden')
-    	loadingDiv = jqnc("<div/>")
-    	loadingDiv.css("position","absolute")
-    	loadingDiv.css("left","190px")
-    	loadingDiv.css("top","250px")
-    	loadingDiv.css("font-size","30px")
-    	loadingDiv.html("loading images..."+imagePreCount+"/"+imgPreloadArray.length)
-		jqnc("body").append(loadingDiv)    	
-    	jqnc(data).find('.imagehighlights [data-img-src]').each(function(){
-    		imgPreloadArray.push(jqnc(this).attr('data-img-src'));
-    	});
-        //console.log('imgPreloadArray '+ imgPreloadArray) ;
-    	for(var pId = 0; pId < imgPreloadArray.length; pId++)
-		{
+
+	function preloadImage() {
+		jqnc(data).css('visibility', 'hidden')
+		loadingDiv = jqnc("<div/>")
+		loadingDiv.css("position", "absolute")
+		loadingDiv.css("left", "190px")
+		loadingDiv.css("top", "250px")
+		loadingDiv.css("font-size", "30px")
+		loadingDiv.html("loading images..." + imagePreCount + "/" + imgPreloadArray.length)
+		jqnc("body").append(loadingDiv)
+		jqnc(data).find('.imagehighlights [data-img-src]').each(function () {
+			imgPreloadArray.push(jqnc(this).attr('data-img-src'));
+		});
+		//console.log('imgPreloadArray '+ imgPreloadArray) ;
+		for (var pId = 0; pId < imgPreloadArray.length; pId++) {
 			var img = new Image();
 			img.onload = imagePreloaded;
 			img.src = imgPreloadArray[pId];
 		}
-    }
+	}
 
-	function imagePreloaded()
-	{
+	function imagePreloaded() {
 		imagePreCount++;
-		if(imagePreCount == imgPreloadArray.length)	{
-			activate();			
+		if (imagePreCount == imgPreloadArray.length) {
+			activate();
 		}
-		loadingDiv.html("loading images..."+imagePreCount+"/"+imgPreloadArray.length)						
+		loadingDiv.html("loading images..." + imagePreCount + "/" + imgPreloadArray.length)
 	}
 
 	preloadImage();
 
-	function activate(){
+	function activate() {
 		addButtonEvents()
 		hideAllHighlights()
 		setupPopup()
@@ -53,51 +54,45 @@ var hotspot = function(data){
 	      jqnc(data).find('.ui-slider-handle').addClass('customPlayhead').addClass('tabindex');
 	      jqnc(data).find('.ui-slider,.ui-slider-range').addClass('sliderRange');
 	    },100);*/
-	    setTimeout(function(){			
-			myScroll = new IScroll('#zoomWrapper',{
-                keyBindings: true,
-                scrollbars: true,
-                scrollX: true,
-                scrollY: true,
-               	zoomMin: 1,
-            	zoomMax: 2,
-                zoom: true
+		setTimeout(function () {
+			myScroll = new IScroll('#zoomWrapper', {
+				keyBindings: true,
+				scrollbars: true,
+				scrollX: true,
+				scrollY: true,
+				zoomMin: 1,
+				zoomMax: 2,
+				zoom: true
 			});
 			jqnc(loadingDiv).remove()
-			jqnc(data).css('visibility','visible')
-		},100);
-
+			jqnc(data).css('visibility', 'visible')
+		}, 100);
 		updateAriaAttributes();
-		
-	}	
+	}
 
 	function addImageAltText() {
 		var altTextStr = 'This is an image of -imgtitle-, which includes self-assessment for the following items; -listlabels-.';
 		var listStr = "";
 		var iCtr = 1;
-		var listLbl = jqnc("#popup_text_"+iCtr);
-		while (listLbl.length>0)
-		{
+		var listLbl = jqnc("#popup_text_" + iCtr);
+		while (listLbl.length > 0) {
 			listLblTxt = jqnc.trim(listLbl.text());
 			++iCtr;
-			listLbl = jqnc("#popup_text_"+iCtr);
+			listLbl = jqnc("#popup_text_" + iCtr);
 
-			if (listLbl.length>0)
-			{
-				listStr += listLblTxt+", ";
-			}else{
+			if (listLbl.length > 0) {
+				listStr += listLblTxt + ", ";
+			} else {
 				// listStr = listStr.substring(0, listStr.length-2);
 				listStr = listStr + 'and ' + listLblTxt;
 			}
-			
+
 		}
 		altTextStr = altTextStr.replace("-imgtitle-", document.title);
 		altTextStr = altTextStr.replace("-listlabels-", listStr);
 		jqnc(".w-hotspot-img").attr("alt", altTextStr);
 		//jqnc("#colorimg").attr("alt", altTextStr);
 	}
-
-	
 	/*function setupSlider(){
 		jqnc(data).find('#slider').slider({
 		    orientation: 'horizontal',
@@ -116,169 +111,190 @@ var hotspot = function(data){
 		}
 	}*/
 
-	function showText(txt,position,align, element) {
+	function showText(txt, position, align, element) {
 		var left = position.left;
-		var top = position.top-5;
-		if(align == 'Left')
-		{
-			left = left-hotspotWidth;
+		var top = position.top - 5;
+		if (align == 'Left') {
+			left = left - hotspotWidth;
+		} else {
+			left = left + hotspotSpaceLeft;
 		}
-		else
-		{
-			left = left+hotspotSpaceLeft;
-		}
-		txtdata.css({'position':'absolute','left':left,'top':top*scale,'width':hotspotWidth,'height':50})
+		txtdata.css({
+			'position': 'absolute',
+			'left': left,
+			'top': top * scale,
+			'width': hotspotWidth,
+			'height': 50
+		})
 		txtdata.html(txt)
-		
-	}	
 
-	function setupPopup()
-	{
+	}
+
+	function setupPopup() {
 		//jqnc('.hotspot .popup').css('position','absolute')
 	}
 
-	function addButtonEvents()
-	{
-		jqnc(data).find('.hotspot > div').bind('click keyup touchstart',onHotSpotClicked);	
-		jqnc(data).find('.hotspot > button').bind('click touchstart',onHotSpotClicked);	
+	function addButtonEvents() {
+		//jqnc(data).find('.hotspot > div').bind('click keyup touchstart',onHotSpotClicked);	
+		jqnc(data).find('.hotspot > button').bind('click touchstart', onHotSpotClicked);
 
-		jqnc(data).find('.hotspot > div').each(function(){
+		/*jqnc(data).find('.hotspot > div').each(function(){
 			jqnc(this).attr('aria-label', jqnc(this).attr('data-popup'));
+		});*/
+		// Move labels ONLY if they are still inside .labelsdata (scoped)
+		jqnc(data).find('.hotspot > button').each(function () {
+			var id = jqnc(this).attr('data-id');
+			var $label = jqnc(data).find('#popup_text_' + id);
+
+			if ($label.length && $label.closest('.labelsdata').length) {
+				jqnc(this).after($label);
+			}
 		});
-		jqnc(data).find('.hotspot > button').each(function(){
-			jqnc(this).attr('aria-label', jqnc(this).attr('data-popup'));
+		// Remove .labelsdata if empty
+		jqnc(data).find('.labelsdata').each(function () {
+			if (jqnc(this).children().length === 0) {
+				jqnc(this).remove();
+			}
 		});
-		jqnc(data).find('.commentButton').bind('click keyup touchstart',onCommentClicked);
-		jqnc(data).find('.close').bind('click keyup touchstart',onCommentCloseClicked);
+		jqnc(data).find('.hotspot > button').each(function () {
+			//jqnc(this).attr('aria-label', jqnc(this).attr('data-popup'));
+			jqnc(this).attr('aria-label', "label " + jqnc(this).attr('data-id'));
+			jqnc(this).attr('aria-expanded', "false");
+			jqnc(this).removeAttr('aria-describedby');
+		});
+		jqnc(data).find('.commentButton').bind('click keyup touchstart', onCommentClicked);
+		jqnc(data).find('.close').bind('click keyup touchstart', onCommentCloseClicked);
 	}
 
-	function onCommentClicked(e)
-	{
-		if(e.type == 'keyup' && (e.keyCode != 13))
+	function onCommentClicked(e) {
+		if (e.type == 'keyup' && (e.keyCode != 13))
 			return false;
 		jqnc(data).find('.modalbg').addClass('modalbgAnimate');
-		jqnc(data).find('.openModal').css('pointer-events','auto');
+		jqnc(data).find('.openModal').css('pointer-events', 'auto');
 		setTimeout(function () {
 			jqnc(data).find('#dialogClose').focus();
 			console.log('focus set')
-		},1000);
+		}, 1000);
 	}
-
-
-	function onCommentCloseClicked(e)
-	{
-		if(e.type == 'keyup' && (e.keyCode != 13) && (e.keyCode != 27))
+	function onCommentCloseClicked(e) {
+		if (e.type == 'keyup' && (e.keyCode != 13) && (e.keyCode != 27))
 			return false;
 		jqnc(data).find('.modalbg').removeClass('modalbgAnimate')
-		jqnc(data).find('.openModal').css('pointer-events','none')
+		jqnc(data).find('.openModal').css('pointer-events', 'none')
 	}
-
-	function setFocus(){
+	function setFocus() {
 		console.log('setFocus call');
-		if(lastFocus != null)
-		{
+		if (lastFocus != null) {
 			console.log('setFocus inside');
-		jqnc(lastFocus).attr('tabindex', -1);	
+			jqnc(lastFocus).attr('tabindex', -1);
 
-		jqnc(lastFocus).focus();	
-		jqnc(lastFocus).attr('tabindex', 0);	
+			jqnc(lastFocus).focus();
+			jqnc(lastFocus).attr('tabindex', 0);
 		}
-		
+
 	}
 
-	function hideAllHighlights(){
-		jqnc(data).find('.imagehighlights > div').each(function(){
+	function hideAllHighlights() {
+		jqnc(data).find('.imagehighlights > div').each(function () {
 			var id = jqnc(this).attr('data-id')
-			if((id != 'bw') &&(id != 'color')&&(id != 'lines')){
+			if ((id != 'bw') && (id != 'color') && (id != 'lines')) {
 				jqnc(this).hide();
 			}
 		})
 		console.log("remove selected 1");
 		jqnc(data).find('.hotspot > div').removeClass('selected');
 		//jqnc(data).find('.hotspot > div').addClass('notselected').attr("aria-pressed",false).attr("aria-expanded", false);
-		jqnc(data).find('.hotspot > div').addClass('notselected').attr("aria-pressed",false);
+		jqnc(data).find('.hotspot > div').addClass('notselected').attr("aria-pressed", false);
 
-		jqnc(data).find('.hotspot > button').removeClass('selected');
+		jqnc(data).find('.hotspot > button').removeClass('selected').attr("aria-expanded", "false").removeAttr("aria-describedby");
 		//jqnc(data).find('.hotspot > div').addClass('notselected').attr("aria-pressed",false).attr("aria-expanded", false);
-		jqnc(data).find('.hotspot > button').addClass('notselected').attr("aria-pressed",false);
+		jqnc(data).find('.hotspot > button').addClass('notselected').attr("aria-pressed", false);
 	}
-	function showSelectedHigh(num)
-	{
+
+	function showSelectedHigh(num) {
 		//alert('showSelectedHigh');
 
-		jqnc(data).find('.imagehighlights > div').each(function(){
+		jqnc(data).find('.imagehighlights > div').each(function () {
 			var id = jqnc(this).attr('data-id')
-			if(id == 'color'){
+			if (id == 'color') {
 				jqnc(this).hide();
 			}
 		});
 
-		jqnc(data).find('.imagehighlights > div').each(function(){
+		jqnc(data).find('.imagehighlights > div').each(function () {
 			var id = jqnc(this).attr('data-id')
-			if(id == num){
+			if (id == num) {
 				jqnc(this).show();
 			}
 		});
 	}
-	function onHotSpotClicked(e)
-	{
-		if(e.type == 'keyup' && (e.keyCode != 13))
+
+	function onHotSpotClicked(e) {
+		if (e.type == 'keyup' && (e.keyCode != 13))
 			return false;
 		lastFocus = jqnc(this);
 		console.log('lastFocus', lastFocus)
 
-		if(!jqnc(e.target).hasClass("selected")){
+		if (!jqnc(e.target).hasClass("selected")) {
 			hideAllHighlights()
 			showSelectedHigh(jqnc(e.target).html());
-			jqnc(e.target).find('.hotspot > div').addClass('notselected')
+			//jqnc(e.target).find('.hotspot > div').addClass('notselected')
 			jqnc(e.target).find('.hotspot > button').addClass('notselected')
 			//jqnc(e.target).addClass('selected').attr("aria-expanded",true).attr("aria-pressed", true)
-			jqnc(e.target).addClass('selected').attr("aria-pressed", true)
-			jqnc(data).find('.hotspot_popup').hide().attr("aria-hidden","true");
-			jqnc('#popup_text_'+jqnc(e.target).attr('data-id')).show().attr("aria-hidden","false");
-		}
-		else{
+			jqnc(data).find('.hotspot_popup').hide().attr("aria-hidden", "true");
+
+			jqnc(e.target).addClass('selected').attr("aria-pressed", true).attr("aria-expanded", "true");
+			jqnc(e.target).attr("aria-describedby", 'popup_text_' + jqnc(e.target).attr('data-id'));
+			jqnc('#popup_text_' + jqnc(e.target).attr('data-id')).show().attr("aria-hidden", "false");
+		} else {
 			hideAllHighlights()
-			jqnc(data).find('.hotspot_popup').hide().attr("aria-hidden","true");
+			jqnc(data).find('.hotspot_popup').hide().attr("aria-hidden", "true");
 		}
 	}
 
-    function setParameters(){
-    	var imgWidth = jqnc(data).find('.w-hotspot-img').width();
+	function setParameters() {
+		var imgWidth = jqnc(data).find('.w-hotspot-img').width();
 		var imgHeight = jqnc(data).find('.w-hotspot-img').height();
-		jqnc(data).find('.imageContainer').css({'width': imgWidth+'px','height': '0px','padding-bottom': imgHeight+'px'})
+		jqnc(data).find('.imageContainer').css({
+			'width': imgWidth + 'px',
+			'height': '0px',
+			'padding-bottom': imgHeight + 'px'
+		})
 		//txtdata.html('');
 		//jqnc(data).find('.hotspot > div').removeClass('selected');
 		//jqnc(data).find('.commentButton').css({'bottom':-imgHeight+'px' });
-		jqnc(data).find('.commentButton').css({'display':'inline' });
-		jqnc(data).find('.imagehighlights > div').css({'height':imgHeight+'px' });
-		jqnc(data).find('.imagehighlights').css({'height':(imgHeight)+'px','width':'90%'});
+		jqnc(data).find('.commentButton').css({
+			'display': 'inline'
+		});
+		jqnc(data).find('.imagehighlights > div').css({
+			'height': imgHeight + 'px'
+		});
+		//jqnc(data).find('.imagehighlights').css({'height':(imgHeight)+'px','width':'90%'});
 
 		if ((innerWidth > 0) && (innerWidth <= 340)) {
-            hotspotSpaceLeft = 20;
-            hotspotWidth = 70;
-        } 
-        else if ((innerWidth > 340) && (innerWidth <= 500)) {
-            hotspotSpaceLeft = 30;
-            hotspotWidth = 100;
-        } 
-        else {
-            hotspotSpaceLeft = 40;
-            hotspotWidth = 150;
-        }
-    }
-	jqnc(document).ready(function(){
+			hotspotSpaceLeft = 20;
+			hotspotWidth = 70;
+		} else if ((innerWidth > 340) && (innerWidth <= 500)) {
+			hotspotSpaceLeft = 30;
+			hotspotWidth = 100;
+		} else {
+			hotspotSpaceLeft = 40;
+			hotspotWidth = 150;
+		}
+	}
+	jqnc(document).ready(function () {
 		//setParameters();
 		//alert(jqnc(window).innerWidth());
 	});
 
-	jqnc(window).resize(function(){
+	jqnc(window).resize(function () {
 		//setParameters();
 	});
 
-	function updateAriaAttributes(){
-		const elm_htspots = document.querySelectorAll("#zoomContainer .hotspot div");
-		const elm_htspotslbls = document.querySelectorAll("#zoomContainer .labelsdata div");
+	function updateAriaAttributes() {
+		debugger;
+		const elm_htspots = document.querySelectorAll("#zoomContainer .hotspot button");
+		const elm_htspotslbls = document.querySelectorAll("#zoomContainer .hotspot div.hotspot_popup");
 		elm_htspots.forEach(elmht => {
 			elmht.setAttribute("role", "button");
 		});
--- Golden_ePub_ENHANCED_v2024.e2/OEBPS/widgets/Flashcard/activity-main/js/widgetinit.js
+++ Golden_ePub_ENHANCED_v2024.e2/OEBPS/widgets/Flashcard/activity-main/js/widgetinit.js
@@ -1,27 +1,37 @@
-(function(app){
-	
-	/*var oActivityArray = ['StepSlider','VideoGallery','','']*/
-	var finalActivityArr = new Array()
+(function(app) {
+    // 1. Make the array public so you can access it via oWidgets.finalActivityArr
+    app.finalActivityArr = [];
 
+    function createActivity() {
+        // 2. Select only relevant elements (Massive performance gain)
+        const elements = document.querySelectorAll('[class^="w-"], [class*=" w-"]');
 
-	function createActivity(){
+        elements.forEach(el => {
+            // 3. Extract the class that starts with 'w-'
+            const widgetClass = Array.from(el.classList).find(cls => cls.startsWith('w-'));
+            
+            if (widgetClass) {
+                const className = widgetClass.split('w-')[1];
+                
+                // 4. Look for the constructor (e.g., window.StepSlider)
+                const WidgetConstructor = window[className];
+                
+                if (typeof WidgetConstructor === 'function') {
+                    // 5. Initialize and store in the public array
+                    const oActivity = new WidgetConstructor(jqnc(el));
+                    app.finalActivityArr.push(oActivity);
+                } else {
+                    console.warn(`Widget class "${className}" not found in window.`);
+                }
+            }
+        });
+    }
 
+    // 6. Use the fastest ready event
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', createActivity);
+    } else {
+        createActivity(); // DOM already loaded
+    }
 
-	jqnc('div').each(function(){
-
-		if(String(jqnc(this).attr('class')).indexOf('w-') != -1)
-		{
-			var activityClass = jqnc(this).attr('class').split('w-')[1]
-			var oActivity = new window[activityClass](jqnc(this))
-			finalActivityArr.push(oActivity)
-		}
-	})
-		
-	}
-
-	jqnc('document').ready(function(){
-		createActivity();
-	})
-
-})(oWidgets = oWidgets || {})
-var oWidgets
+})(window.oWidgets = window.oWidgets || {});
